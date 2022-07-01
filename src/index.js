233d4f20b613e7cd3d149bdaacb658522c4dea9f
Cesar August <45099916+CesarAugst@users.noreply.github.com>
2022-07-01 20:31:34 -0300

    middleware e rota create /users
    Middleware implementado e rota users usando verbo post para criação de usuarios

--- src/index.js
+++ src/index.js
@@ -1,25 +1,51 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { id } = request.headers; //recupera id do request
+  const user = users.find((user) => user.id === id); //bool se existe
+
+  if(!user){
+    return response.status(400).json({message: "Não encontrado"});//se não encontrar
+  }
+
+  request.user = user; //disponibiliza para as rotas o usuario
+  return next(); //se encontrar e não cair na condicional
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body; //pega do request
+  const id = uuidv4(); //cria id como uuid
+
+  if(name == null || username == null)
+  response.status(400).json({message: "faltam parametros"});//não procegue se faltar dados
+
+  users.push({
+    id,
+    name,
+    username
+  }); //faz o incremento do array
+
+  return response.status(201).json({
+    message: "Criado com sucesso",
+    user: {
+      id,
+      name,
+      username
+    }
+  }); //envia a resposta
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
